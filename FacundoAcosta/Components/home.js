f80ca08088bdc458c0098a042aa8e422cfca2ef9
Facundo <acosta.facundo.h@gmail.com>
2021-07-14 20:51:17 -0300

    Handling errors

--- FacundoAcosta/Components/home.js
+++ FacundoAcosta/Components/home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { charactersContext } from './rick&mortyContext';
 import CharacterList from './charactersList';
@@ -6,20 +6,12 @@ import CharacterList from './charactersList';
 
 function Home() {
 
-    const [characters , setCharacters ] = useContext(charactersContext);
-
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character')
-        .then(res => res.json())
-        .then(data => setCharacters(data.results))
-    }, [])
-    
-
     return (
-        <View>
-            <CharacterList />
-        </View>
-    )
+    <View>
+        <CharacterList />
+    </View>
+    ) 
+    
 }
 
 export default Home;
