68a4875feaba7031ca04269dfab3a1ddaaaf39ba
Facundo <acosta.facundo.h@gmail.com>
2021-07-14 18:03:33 -0300

    Added Drawer Navigation

--- FacundoAcosta/App.js
+++ FacundoAcosta/App.js
@@ -5,32 +5,48 @@ import Home from './Components/home';
 import { CharactersProvider } from './Components/rick&mortyContext';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import CharacterDetails from './Components/characterDetails';
+import RandomList from './Components/randomList';
 
 const Stack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+function StackNav() {
+  
+  return (
+    <Stack.Navigator initialRouteName="Rick and Morty">
+      <Stack.Screen 
+        name="Rick and Morty" 
+        component={Home}
+        options={{
+          title: 'Rick & Morty',
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: 'bold',
+            marginLeft: 120,
+          }
+        }} />
+      <Stack.Screen 
+        name="characterDetails" 
+        component={CharacterDetails} 
+        options={({ route }) => ({ title: route.params.name })} />
+    </Stack.Navigator>
+  )
+}
+
 
 function App() {
   
   return (
     <CharactersProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Rick and Morty">
-          <Stack.Screen 
-            name="Rick and Morty" 
-            component={Home}
-            options={{
-              title: 'Rick & Morty',
-              headerTitleStyle: {
-                fontSize: 25,
-                fontWeight: 'bold',
-                marginLeft: 120,
-              }
-            }} />
-          <Stack.Screen 
-            name="characterDetails" 
-            component={CharacterDetails} 
-            options={({ route }) => ({ title: route.params.name })} />
-        </Stack.Navigator>
+        <Drawer.Navigator>
+          <Drawer.Screen name='Drawer/home' component={StackNav}/>
+          <Drawer.Screen name='All Locations' component={RandomList} />
+          <Drawer.Screen name='All Episodes' component={RandomList} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </CharactersProvider>
   )
