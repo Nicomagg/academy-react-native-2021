c994d0cd8b8312753e9454029ef9c5db499d9ecd
Mauro Luis Vargas <luisvargas.tbd@gmail.com>
2021-07-14 16:34:49 -0300

    making changes

--- luchi-rym/src/App.js
+++ luchi-rym/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container">
+      <header>
+        <h1>
+          Rick & Morty's Simple Wiki <small>by Luchi</small>
+        </h1>
+      </header>
+      <body>
+        <section>
+          This is a simple WebApp which permits you to find any character from
+          famous Rick & Morty animated show. Feel free to navigate throughout
+          sections to find the info you want to know. Leave any feedback in
+          contact section so we can get in touch about this or any topic.
+        </section>{" "}
+      </body>
+      <footer>All rights reserved </footer>{" "}
+    </div>
+  );
 }
 
 export default App;
