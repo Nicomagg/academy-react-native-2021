5a666c17b72aed068a4dd0cc6a65097820b35c9d
Facundo <acosta.facundo.h@gmail.com>
2021-07-14 15:38:56 -0300

    Character Preview added with styles

--- FacundoAcosta/Components/charactersList.js
+++ FacundoAcosta/Components/charactersList.js
@@ -1,30 +1,42 @@
 import React, { useContext } from 'react';
-import { View , Text , FlatList } from 'react-native';
+import { View , StyleSheet , FlatList, TouchableHighlight } from 'react-native';
 import { charactersContext } from './rick&mortyContext';
-
-function CharacterPreView({ character }) {
-    return (
-        <Text>{character.name}</Text>
-    );
-}
+import CharacterPreView from './characterPreView'
 
 function CharacterList() {
 
     const characters = useContext(charactersContext)[0];
 
     const renderCharacter = ({item}) => (
-        <CharacterPreView character={item}/>
+        <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor='#DDDDDD'
+            >
+            <CharacterPreView character={item}/>
+        </TouchableHighlight>
     )
-
+    
+    const itemSeparator = () => {
+        return (
+            <View style={{height:10, width: '100%'}} />
+        )
+    }
     return (
-        <View>
+        <View style={styles.container}>
             <FlatList 
                 data={characters}
+                ItemSeparatorComponent={itemSeparator}
+                ListFooterComponent={itemSeparator}
+                ListHeaderComponent={itemSeparator}
                 renderItem={renderCharacter}
                 keyExtractor={character => character.id} 
             />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+
+})
 
 export default CharacterList;
--- FacundoAcosta/Components/home.js
+++ FacundoAcosta/Components/home.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { charactersContext } from './rick&mortyContext';
 import CharacterList from './charactersList';
 
