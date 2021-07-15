ca4db2fe3cd8269f61f49b2214f480c63304c8dc
Heli <wurch.heliana@gmail.com>
2021-07-14 22:53:29 -0300

    [F01][HW02]Connect to Api - Characters

--- HelianaWurchApp/App.js
+++ HelianaWurchApp/App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
