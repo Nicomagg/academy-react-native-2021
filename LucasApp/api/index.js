4ed00ca8c55e09e8f4d85d9f831e5aedd6b7c455
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-07-15 16:57:00 -0300

    Create getAllCharacters function

---
+++ LucasApp/api/index.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://rickandmortyapi.com/api';
+
+export default {
+  getAllCharacters: () =>
+    fetch(`${BASE_URL}/character`)
+      .then(response => response.json())
+      .then(data => data.results),
+};
