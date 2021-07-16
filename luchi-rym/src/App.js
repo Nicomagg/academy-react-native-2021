34c743c7478e23ef9b87f63d321a1aba0114c218
Mauro Luis Vargas <luisvargas.tbd@gmail.com>
2021-07-15 23:30:15 -0300

    Basic UI mapping

--- luchi-rym/src/App.js
+++ luchi-rym/src/App.js
@@ -1,28 +1,43 @@
 import React, { useState, useEffect, useReducer } from "react";
 import "./App.css";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { Button } from "reactstrap";
 
 function Welcome() {
   return (
-    <p>
-      This is a simple WebApp which permits you to find any character from
-      famous Rick & Morty animated show. Push the button to see all characters
-      or search anything in particular.
-    </p>
+    <>
+      <div className="box">
+        <form>
+          <input
+            className="form-control src"
+            name="search"
+            name="search"
+            type="text"
+            placeholder="Search Anything"
+          />
+        </form>
+      </div>
+      <div>
+        <p>
+          This is a simple WebApp which permits you to find any character from
+          famous Rick & Morty animated show. Push the button to see all
+          characters or search anything in particular.
+        </p>
+      </div>
+    </>
   );
 }
 
 function App() {
   return (
     <div className="container">
-      <body>
+      <main>
         <Welcome />
         <div className="princ">
           <div className="sec">
             <Button color="success">Show All Characters</Button>
           </div>
         </div>{" "}
-      </body>
+      </main>
     </div>
   );
 }
--- luchi-rym/src/index.js
+++ luchi-rym/src/index.js
@@ -5,9 +5,36 @@ import App from "./App";
 function Enc() {
   return (
     <header>
-      <h1>
-        Rick & Morty's Simple Wiki <small>by Luchi</small>
-      </h1>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Rick & Morty <small>Wiki</small>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample01"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarsExample01">
+            <ul className="navbar-nav me-auto mb-2">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  All Locations
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  All Episodes
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
