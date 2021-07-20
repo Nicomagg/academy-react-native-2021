667a1fd5dd150484d508e9caa0fc57421d326b68
Lucas Maidana <lucasmaidanasoy@gmail.com>
2021-07-20 16:51:20 -0300

    Created getAllLocations function

--- LucasApp/pages/AllLocations.js
+++ LucasApp/pages/AllLocations.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, ActivityIndicator} from 'react-native';
+import api from './../api';
 
 const AllLocations = () => {
+  const [status, setStatus] = useState('LOADING');
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    api
+      .getAllLocations()
+      .then(response => {
+        setData(response);
+        setStatus('RESOLVED');
+      })
+      .catch(() => setStatus('REJECTED'));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>All Locations</Text>
+      {status === 'LOADING' && (
+        <ActivityIndicator size="large" color="#999999" />
+      )}
+      {status === 'RESOLVED' && <Text>{JSON.stringify(data)}</Text>}
+      {status === 'REJECTED' && (
+        <Text>Error loading data. Please try again later.</Text>
+      )}
     </View>
   );
 };
